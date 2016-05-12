501418728c9567b2ae9dc71783d4a742affae93f
陈钰辉 <chenyh@shinemo.com>
2016-05-12 15:21:59 +0800

    bugfix

--- java-demo/openapi-demo-sdk/src/main/java/com/shinemo/openapi/demo/sdk/accesstoken/AccessTokenDemo.java
+++ java-demo/openapi-demo-sdk/src/main/java/com/shinemo/openapi/demo/sdk/accesstoken/AccessTokenDemo.java
@@ -6,6 +6,7 @@ import com.shinemo.openapi.java.sdk.client.OpenapiClient;
 import com.shinemo.openapi.java.sdk.exception.OpenapiException;
 import com.shinemo.openapi.java.sdk.request.AccessTokenRequest;
 import com.shinemo.openapi.java.sdk.response.AccessTokenResponse;
+import com.shinemo.openapi.java.sdk.response.GetUserInfoByTokenResponse;
 
 /**
  * 获取accessToken demo
@@ -13,15 +14,18 @@ import com.shinemo.openapi.java.sdk.response.AccessTokenResponse;
 public class AccessTokenDemo {
 
     public static void main(String[] args){
-        AccessTokenRequest request = new AccessTokenRequest("429796111","7511B4A213A7710C");
+        AccessTokenRequest request = new AccessTokenRequest("43243ewrwerwwerwerewerwerwerew243234","3327d6bf20d1dsfdsfsdfsdfdsfsdfdsfsdfsda08ba6afc294862312fd");
         OpenapiClient client = OpenapiClientFactory.getClient();
         try {
-            AccessTokenResponse response = client.execute(request);
-            System.out.println(response.toString());
+
+                AccessTokenResponse response = client.execute(request);
+                System.out.println(response.toString());
         } catch (OpenapiException e) {
             // TODO handle
+            e.printStackTrace();
         } catch (Exception e){
             // TODO handle
+            e.printStackTrace();
         }
     }
 
--- java-demo/openapi-demo-sdk/src/main/java/com/shinemo/openapi/demo/sdk/userinfo/UserInfoDemo.java
+++ java-demo/openapi-demo-sdk/src/main/java/com/shinemo/openapi/demo/sdk/userinfo/UserInfoDemo.java
@@ -16,12 +16,13 @@ public class UserInfoDemo {
         GetUserInfoByTokenRequest request = new GetUserInfoByTokenRequest("3327d6bf20d1a08ba6afc294862312fd","3327d6bf20d1a08ba6afc294862312fd");
         OpenapiClient client = OpenapiClientFactory.getClient();
         try {
-            GetUserInfoByTokenResponse response = client.execute(request);
-            System.out.println(response.toString());
+                GetUserInfoByTokenResponse response = client.execute(request);
+                System.out.println(response.toString());
         } catch (OpenapiException e) {
             // TODO handle
         } catch (Exception e){
             // TODO handle
+            e.printStackTrace();
         }
     }
 
