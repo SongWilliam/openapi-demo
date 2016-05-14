b7350ba1395746e2fb9046b4e822ec305bdc9773
陈钰辉 <chenyh@shinemo.com>
2016-05-14 15:33:42 +0800

    php hello

---
+++ php-demo/http-demo/config.php
@@ -0,0 +1,5 @@
+<?php
+define('DIR_ROOT', dirname(__FILE__).'/');
+define("OAPI_HOST", "https://api.open.jituancaiyun.con");//根据情况修改,麻绳:https://api.open.imasheng.com
+define("APPID", "");
+define("APPSECRET", "");//必填，在创建应用的时候会分配
